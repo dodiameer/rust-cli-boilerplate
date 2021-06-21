722697416c1c84dd678bb89df0c971a72980b06f
dodiameer <mtxshiftg@gmail.com>
2021-06-21 17:59:56 +0300

    Added error handling

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 mod args;
 use args::Args;
 use structopt::StructOpt;
+use anyhow::{Context, Result, Error};
 
-fn main() {
+fn cause_error(reason: &'static str) -> Result<()> {
+    Err(Error::msg(reason))
+}
+
+fn main() -> Result<()> {
     let args = Args::from_args(); 
     println!("{}", args);
+    cause_error("Error caused to check out error handling").context("`cause_error` called in main function")?;
+    Ok(())
 }
